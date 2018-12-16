9587c2ba88b766d5bdcff947ff04b4b82b5e7912
jasonchien1996 <43934912+jasonchien1996@users.noreply.github.com>
2018-12-17 00:35:25 +0800

    Add files via upload

---
+++ board.h
@@ -0,0 +1,250 @@
+#pragma once
+#include <array>
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <vector>
+/**
+ * array-based board for Threes
+ *
+ * index (1-d form):
+ *  (0)  (1)  (2)
+ *  (3)  (4)  (5)
+ *
+ */
+class board {
+public:
+	typedef uint32_t cell;
+	typedef std::array<cell, 3> row;
+	typedef std::array<row, 2> grid;
+	typedef uint64_t data;
+	typedef int reward;
+
+public:
+	board() : tile(), attr(0), type('a'), direct(-1), bag({1, 2, 3}) {}
+	board(const grid& b, data v = 0) : tile(b), attr(v) {}
+	board(const board& b) = default;
+	board& operator =(const board& b) = default;
+
+	operator grid&() { return tile; }
+	operator const grid&() const { return tile; }
+	row& operator [](unsigned i) { return tile[i]; }
+	const row& operator [](unsigned i) const { return tile[i]; }
+	cell& operator ()(unsigned i) { return tile[i / 3][i % 3]; }
+	const cell& operator ()(unsigned i) const { return tile[i / 3][i % 3]; }
+
+	data info() const { return attr; }
+	data info(data dat) { data old = attr; attr = dat; return old; }
+
+public:
+	bool operator ==(const board& b) const { return tile == b.tile; }
+	bool operator < (const board& b) const { return tile <  b.tile; }
+	bool operator !=(const board& b) const { return !(*this == b); }
+	bool operator > (const board& b) const { return b < *this; }
+	bool operator <=(const board& b) const { return !(b < *this); }
+	bool operator >=(const board& b) const { return !(*this < b); }
+
+public:
+    int get_direct() const{ return direct; }
+
+	/**
+	 * place a tile (index value) to the specific position (1-d form index)
+	 * return 0 if the action is valid, or -1 if not
+	 */
+	reward place(unsigned pos, cell tile) {
+		if (pos >= 6) return -1;
+		if (tile != 1 && tile != 2 && tile!= 3) return -1;
+		operator()(pos) = tile;
+		return 0;
+	}
+
+	/**
+	 * apply an action to the board
+	 * return the reward of the action, or -1 if the action is illegal
+	 */
+	reward slide(unsigned opcode) {
+		switch (opcode & 0b11) {
+		case 0: return slide_up();
+		case 1: return slide_right();
+		case 2: return slide_down();
+		case 3: return slide_left();
+		default: return -1;
+		}
+	}
+
+	reward slide_left()	{
+		board prev = *this;
+		reward score = 0;
+		for (int r = 0; r < 2; r++) {
+			auto& row = tile[r];
+			int hold = 0, blank = 0;
+			for (int c = 0; c < 3; c++) {
+				int tile = row[c];
+				if (tile == 0){blank = 1;}
+                else {
+                    if (hold == tile && hold > 2) {
+                        row[c-1] = ++tile;
+                        blank = 1;
+                    }
+					else if (abs((hold - tile)) == 1 && (hold + tile) == 3) {
+                        row[c-1] = 3;
+                        blank = 1;
+                    }
+                    else
+                        hold = tile;
+                }
+                if(blank == 1) {
+                    for(int i = c; i < 2; i++)
+                        row[i] = row[i+1];
+                    row[2] = 0;
+                    break;
+                }
+			}
+		}
+		direct = 3;
+		if(*this != prev) {
+		    int a = 0, b = 0;
+            for(int i = 0; i < 6; i++) {
+                if(operator ()(i) > 2) {
+                    int power = 1, n = operator ()(i) - 2;
+                    while(n--) power *= 3;
+                    a += power;
+                }
+                if(prev.operator ()(i) > 2) {
+                    int power = 1, n = prev.operator ()(i) - 2;
+                    while(n--) power *= 3;
+                    b += power;
+                }
+            }
+            score = a - b;
+            return score;
+		}
+		return -1;
+	}
+	reward slide_right() {
+		reflect_horizontal();
+		reward score = slide_left();
+		reflect_horizontal();
+		direct = 1;
+		return score;
+	}
+	reward slide_up() {
+		board prev = *this;
+		reward score = 0;
+		for(int c = 0; c <= 2; c++) {
+			int hold = 0, blank = 0;
+			for(int r = 0; r <= 1 ; r++) {
+				int now = tile[r][c];
+				if (now == 0){blank = 1;}
+                else {
+                    if (hold == now && hold > 2) {
+                        tile[r-1][c] = ++now;
+                        blank = 1;
+                    }
+					else if (abs((hold - now)) == 1 && (hold + now) == 3) {
+                        tile[r-1][c] = 3;
+                        blank = 1;
+                    }
+                    else
+                        hold = now;
+                }
+                if(blank == 1) {
+                    for(int i = r; i < 1; i++)
+                        tile[i][c] = tile[i+1][c];
+                     tile[1][c] = 0;
+                    break;
+                }
+			}
+		}
+		direct = 0;
+ 		if(*this != prev) {
+		    int a = 0, b = 0;
+            for(int i = 0; i < 6; i++) {
+                if(operator ()(i) > 2) {
+                    int power = 1, n = operator ()(i) - 2;
+                    while(n--) power *= 3;
+                    a += power;
+                }
+                if(prev.operator ()(i) > 2) {
+                    int power = 1, n = prev.operator ()(i) - 2;
+                    while(n--) power *= 3;
+                    b += power;
+                }
+            }
+            score = a - b;
+            return score;
+ 		}
+		return -1;
+	}
+	reward slide_down() {
+		reflect_vertical();
+		reward score = slide_up();
+		reflect_vertical();
+		direct = 2;
+		return score;
+	}
+
+	/*void transpose() {
+		for (int r = 0; r < 4; r++) {
+			for (int c = r + 1; c < 4; c++) {
+				std::swap(tile[r][c], tile[c][r]);
+			}
+		}
+	}*/
+
+	void reflect_horizontal() {
+		for (int r = 0; r < 2; r++) {
+			std::swap(tile[r][0], tile[r][2]);
+			//std::swap(tile[r][1], tile[r][2]);
+		}
+	}
+
+	void reflect_vertical() {
+		for (int c = 0; c < 3; c++) {
+			std::swap(tile[0][c], tile[1][c]);
+			//std::swap(tile[1][c], tile[2][c]);
+		}
+	}
+
+	/**
+	 * rotate the board clockwise by given times
+	 */
+	/*void rotate(int r = 1) {
+		switch (((r % 4) + 4) % 4) {
+		default:
+		case 0: break;
+		case 1: rotate_right(); break;
+		case 2: reverse(); break;
+		case 3: rotate_left(); break;
+		}
+	}
+
+	void rotate_right() { transpose(); reflect_horizontal(); } // clockwise
+	void rotate_left() { transpose(); reflect_vertical(); } // counterclockwise*/
+
+public:
+	friend std::ostream& operator <<(std::ostream& out, const board& b) {
+		for (int i = 0; i < 6; i++) {
+            int k = (b(i) > 3) ? (1 << (b(i)-3) & -2u)*3 : b(i);
+			out << std::setw(std::min(i, 1)) << "" << k;
+		}
+		return out;
+	}
+	friend std::istream& operator >>(std::istream& in, board& b) {
+		for (int i = 0; i < 6; i++) {
+			while (!std::isdigit(in.peek()) && in.good()) in.ignore(1);
+			in >> b(i);
+			if(b(i) > 3) b(i) = std::log2(b(i)/3)+3;
+		}
+		return in;
+	}
+
+public:
+    char type;
+	std::vector<int> bag;
+
+private:
+    int direct;
+	grid tile;
+	data attr;
+};
---
+++ solver.h
@@ -0,0 +1,345 @@
+#pragma once
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include "board.h"
+#include <numeric>
+#include <vector>
+#include <cstring>
+
+class state_type {
+public:
+	enum type : char {
+		before  = 'b',
+		after   = 'a',
+		illegal = 'i'
+	};
+
+public:
+	state_type() : t(illegal) {}
+	state_type(const state_type& st) = default;
+	state_type(state_type::type code) : t(code) {}
+
+	friend std::istream& operator >>(std::istream& in, state_type& type) {
+		std::string s;
+		if (in >> s) type.t = static_cast<state_type::type>((s + " ").front());
+		return in;
+	}
+
+	friend std::ostream& operator <<(std::ostream& out, const state_type& type) {
+		return out << char(type.t);
+	}
+
+	bool is_before()  const { return t == before; }
+	bool is_after()   const { return t == after; }
+	bool is_illegal() const { return t == illegal; }
+
+//private:
+	type t;
+};
+
+class state_hint {
+public:
+	state_hint(const board& state) : state(const_cast<board&>(state)) {}
+
+	char type() const { return state.info() ? state.info() + '0' : 'x'; }
+	operator board::cell() const { return state.info(); }
+
+public:
+	friend std::istream& operator >>(std::istream& in, state_hint& hint) {
+		while (in.peek() != '+' && in.good()) in.ignore(1);
+		char v; in.ignore(1) >> v;
+		hint.state.info(v != 'x' ? v - '0' : 0);
+		return in;
+	}
+	friend std::ostream& operator <<(std::ostream& out, const state_hint& hint) {
+		return out << "+" << hint.type();
+	}
+
+private:
+	board& state;
+};
+
+class solver {
+public:
+	typedef float value_t;
+
+public:
+    int lut(board state){
+        int index = 0;
+        for(int i = 0; i < 6; i++){
+            int power = 1, n = i;
+            while(n--) power *= 12;
+            index += state.operator ()(i) * power;
+        }
+        return index;
+    }
+
+public:
+	class answer{
+	public:
+		answer(value_t min = 0.0/0.0, value_t avg = 0.0/0.0, value_t max = 0.0/0.0) : min(min), avg(avg), max(max){}
+	    friend std::ostream& operator <<(std::ostream& out, const answer& ans) {
+	    	return !std::isnan(ans.avg) ? (out << ans.min << " " << ans.avg << " " << ans.max) : (out << "-1") << std::endl;
+		}
+	public:
+		value_t min, avg, max;
+	};
+
+    answer build(board before){
+        board after;
+        if(before.type == 'b'){
+            int r, v = 0;
+            int index = lut(before);
+            int hint = before.bag.back();
+            float avg = -99999;
+            float max = 0;
+            float min = 0;
+
+            if(b_v[2985983*(hint-1)+index] == 1){
+                min = b_min[2985983*(hint-1)+index];
+                avg = b_avg[2985983*(hint-1)+index];
+                max = b_max[2985983*(hint-1)+index];
+                return {min, avg, max};
+            }
+
+            for(int i = 0; i < 4; i++){
+                after = before;
+                r = after.slide(i);
+                //legal
+                if(r != -1){
+                    v = 1;
+                    after.type = 'a';
+                    answer current = build(after);
+                    if(avg < current.avg){
+                        avg = current.avg;
+                        max = current.max;
+                        min = current.min;
+                    }
+
+                }
+            }
+            //non-leaf
+            if(v == 1){
+                b_max[2985983*(hint-1)+index] = max;
+                b_min[2985983*(hint-1)+index] = min;
+                b_avg[2985983*(hint-1)+index] = avg;
+                b_v[2985983*(hint-1)+index] = 1;
+                return {min, avg, max};
+            }
+            //leaf
+            else{
+                avg = 0;
+                for(int i = 0; i < 6; i++) {
+                    if(before.operator ()(i) > 2) {
+                        int power = 1, n = before.operator ()(i) - 2;
+                        while(n--) power *= 3;
+                        max += power;
+                        min += power;
+                        avg += power;
+                    }
+                }
+                b_max[2985983*(hint-1)+index] = max;
+                b_min[2985983*(hint-1)+index] = min;
+                b_avg[2985983*(hint-1)+index] = avg;
+                b_v[2985983*(hint-1)+index] = 1;
+                return {min, avg, max};
+            }
+        }
+        else if(before.type == 'a'){
+            int hint = before.bag.back();
+            before.bag.pop_back();
+            if(before.bag.empty()) before.bag = {1,2,3};
+            int last = before.get_direct();
+            int index = lut(before);
+            int num_child = 0;
+            float max = -99999;
+            float min = 99999;
+            float avg = 0;
+
+            if(a_v[index + 2985983 * ((hint-1) + 3 * last)] == 1){
+                min = a_min[index + 2985983 * ((hint-1) + 3 * last)];
+                avg = a_avg[index + 2985983 * ((hint-1) + 3 * last)];
+                max = a_max[index + 2985983 * ((hint-1) + 3 * last)];
+                return {min, avg, max};
+            }
+
+            if(last == 0){
+                for(int pos : up){
+                    if (before(pos) != 0) continue;
+                    after = before;
+                    after.place(pos,hint);
+                    after.type = 'b';
+                    std::vector<int> temp = after.bag;
+                    std::vector<int> t = after.bag;
+                    for(int new_hint : temp){
+                        after.bag = t;
+                        auto match = std::find(after.bag.begin(), after.bag.end(), new_hint);
+                        after.bag.erase(match);
+                        after.bag.push_back(new_hint);
+                        num_child++;
+                        answer current = build(after);
+                        if(max < current.max) max = current.max;
+                        if(min > current.min) min = current.min;
+                        avg += current.avg;
+                    }
+                }
+            }
+            else if(last == 2){
+                for(int pos : down){
+                    if (before(pos) != 0) continue;
+                    after = before;
+                    after.place(pos,hint);
+                    after.type = 'b';
+                    std::vector<int> temp = after.bag;
+                    std::vector<int> t = after.bag;
+                    for(int new_hint : temp){
+                        after.bag = t;
+                        auto match = std::find(after.bag.begin(), after.bag.end(), new_hint);
+                        after.bag.erase(match);
+                        after.bag.push_back(new_hint);
+                        num_child++;
+                        answer current = build(after);
+                        if(max < current.max) max = current.max;
+                        if(min > current.min) min = current.min;
+                        avg += current.avg;
+                    }
+                }
+            }
+            else if(last == 3){
+                for(int pos : left){
+                    if (before(pos) != 0) continue;
+                    after = before;
+                    after.place(pos,hint);
+                    after.type = 'b';
+                    std::vector<int> temp = after.bag;
+                    std::vector<int> t = after.bag;
+                    for(int new_hint : temp){
+                        after.bag = t;
+                        auto match = std::find(after.bag.begin(), after.bag.end(), new_hint);
+                        after.bag.erase(match);
+                        after.bag.push_back(new_hint);
+                        num_child++;
+                        answer current = build(after);
+                        if(max < current.max) max = current.max;
+                        if(min > current.min) min = current.min;
+                        avg += current.avg;
+                    }
+                }
+            }
+            else if(last == 1){
+
+                for(int pos : right){
+                    if (before(pos) != 0) continue;
+                    after = before;
+                    after.place(pos,hint);
+                    after.type = 'b';
+                    std::vector<int> temp = after.bag;
+                    std::vector<int> t = after.bag;
+                    for(int new_hint : temp){
+                        after.bag = t;
+                        auto match = std::find(after.bag.begin(), after.bag.end(), new_hint);
+                        after.bag.erase(match);
+                        after.bag.push_back(new_hint);
+                        num_child++;
+                        answer current = build(after);
+                        if(max < current.max) max = current.max;
+                        if(min > current.min) min = current.min;
+                        avg += current.avg;
+                    }
+                }
+            }
+            a_max[index + 2985983 * ((hint-1) + 3 * last)] = max;
+            a_min[index + 2985983 * ((hint-1) + 3 * last)] = min;
+            a_avg[index + 2985983 * ((hint-1) + 3 * last)] = avg/num_child;
+            a_v[index + 2985983 * ((hint-1) + 3 * last)] = 1;
+            return {min, avg/num_child, max};
+        }
+    }
+
+public:
+	solver(const std::string& args) {
+		// TODO: explore the tree and save the result
+        board init_board;
+        board after;
+        answer current;
+        float max = -99999;
+        float min = 99999;
+        float avg = 0;
+
+        for(int i = 1; i <= 3; i++){
+            for(int j = 0; j <= 5; j++){
+                after = init_board;
+                after.place(j, i);
+                after.type = 'b';
+                auto match = std::find(after.bag.begin(), after.bag.end(), i);
+                after.bag.erase(match);
+                std::vector<int> temp = after.bag;
+                std::vector<int> t = after.bag;
+                for(int hint : temp){
+                    after.bag = t;
+                    auto match = std::find(after.bag.begin(), after.bag.end(), hint);
+                    after.bag.erase(match);
+                    after.bag.push_back(hint);
+                    current = build(after);
+                    if(max < current.max) max = current.max;
+                    if(min > current.min) min = current.min;
+                    avg += current.avg;
+                }
+            }
+            a_max[0 + 2985983 * ((i - 1) + 3 * 0)] = max;
+            a_min[0 + 2985983 * ((i - 1) + 3 * 0)] = min;
+            a_avg[0 + 2985983 * ((i - 1) + 3 * 0)] = avg/12;
+            a_v[0 + 2985983 * ((i - 1) + 3 * 0)] = 1;
+        }
+	}
+
+	answer solve(const board& state, state_type type = state_type::before) {
+		// TODO: find the answer in the lookup table and return it
+		//       do NOT recalculate the tree at here
+        int index = lut(state);
+		// to fetch the hint (if type == state_type::after, hint will be 0)
+		board::cell hint = state_hint(state);
+		//check for legality
+
+		// for a legal state, return its three values.
+//		return { min, avg, max };
+        if(type.t == 'b'){
+            if(b_v[2985983*(hint-1)+index] == 1){
+                float min = b_min[2985983*(hint-1)+index];
+                float avg = b_avg[2985983*(hint-1)+index];
+                float max = b_max[2985983*(hint-1)+index];
+                return { min, avg, max };
+            }
+        }
+        else if(type.t == 'a'){
+            for(int i = 0; i < 4; i++){
+                if(a_v[index + 2985983 * ((hint-1) + 3 * i)] == 1){
+                    float min = a_min[index + 2985983 * ((hint-1) + 3 * i)];
+                    float avg = a_avg[index + 2985983 * ((hint-1) + 3 * i)];
+                    float max = a_max[index + 2985983 * ((hint-1) + 3 * i)];
+                    return { min, avg, max };
+                }
+            }
+        }
+		// for an illegal state, simply return {}
+		return {};
+	}
+
+public:
+	const int up[3] = {3, 4, 5};
+	const int down[3] = {0, 1, 2};
+    const int left[2] = {2, 5};
+    const int right[2] = {0, 3};
+
+private:
+	// TODO: place your transposition table here
+	float *b_max = new float[3*2985983];
+	float *b_min = new float[3*2985983];
+	float *b_avg = new float[3*2985983];
+	int *b_v = new int[3*2985983]();
+	float *a_max = new float[3*2985983*4];
+	float *a_min = new float[3*2985983*4];
+	float *a_avg = new float[3*2985983*4];
+	int *a_v = new int[3*2985983*4]();
+};
